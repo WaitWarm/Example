c4754234318c47aeaa8fdba95bda965342d073ac
WaitWarm <313094472@qq.com>
2018-10-11 16:51:25 +0800

    添加了示例代码，完成

--- venv/bayes.py
+++ venv/bayes.py
@@ -67,6 +67,44 @@ def bagOfWords2VecMN(vocabList,inputSet):
             returnVec[vocabList.index(word)]+=1
     return returnVec
 
+def textParse(bigString):
+    import re
+    listOfTokens=re.split(r'\w*',bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok)>2]
+
+def spamTest():
+    docList=[]
+    classList=[]
+    fullText=[]
+    for i in range(1,26):
+        wordList=textParse(open('email/spam/%d.txt' %i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList=textParse(open('email/ham/%d.txt' %i).read())
+        docList.append(wordList)
+        fullText.append(wordList)
+        classList.append(0)
+    vocabList=createVocabList(docList)
+    trainingSet=list(range(50))
+    testSet=[]
+    for i in range(10):
+        randIndex=int(random.uniform(0,len(trainingSet)))#随机生成下一个实数，它在 [x, y) 范围内。
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+    trainMat=[]
+    trainClasses=[]
+    for docIndex in trainingSet:
+        trainMat.append(setOfWords2Vec(vocabList,docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V,p1V,pSpam=trainNB0(array(trainMat),array(trainClasses))
+    errorCount=0
+    for docIndex in testSet:
+        wordVector=setOfWords2Vec(vocabList,docList[docIndex])
+        if classifyNB(array(wordVector),p0V,p1V,pSpam)!=classList[docIndex]:
+            errorCount+=1
+    print('the error rate is:',float(errorCount)/len(testSet))
+
 # listOPosts,listClasses=loadDataSet()
 # myVocabList=createVocabList(listOPosts)
 # trainMat=[]
@@ -77,4 +115,5 @@ def bagOfWords2VecMN(vocabList,inputSet):
 # print(p0V)
 # print(p1V)
 # print(pAb)
-testingNB()
+#testingNB()
+spamTest()
